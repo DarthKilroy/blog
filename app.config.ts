faca94acf4d4e32eeaa430d31f38ed911d266cfe
Darth Kilroy <darthkilroy@sturtz.io>
2023-07-27 00:51:22 +0000

    Updated Footer

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'I am Kilroy',
+    description: 'A blog about anarchy, hacking and what ever pops into my head randomly.',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
@@ -18,21 +18,11 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        enabled: false, // possible value are : true | false
+        repository: 'https://git.sturtz.io/darthkilroy/blog'
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
-    },
-    socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
-      linkedin: {
-        icon: 'uil:linkedin',
-        label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
     },
     form: {
       successMessage: 'Message sent. Thank you!'
